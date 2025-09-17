fc77b5ae3c32815c2acc26e99c4ebf0b9578ce1b
viannik <ivan.nikitski@gmail.com>
2025-09-17 23:10:22 +0300

    user can create orders for itself

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -2,9 +2,13 @@ from rest_framework import viewsets
 
 from .models import Order
 from .serializers import OrderSerializer, OrderListSerializer
+from apps.permissions import IsAuthenticatedOrAdmin
 
 class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+    permission_classes = [IsAuthenticatedOrAdmin]
+
     def get_queryset(self):
         user = self.request.user
         if user.is_staff:
